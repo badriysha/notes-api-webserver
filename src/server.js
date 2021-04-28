711ad18cbcb9f73cfa9bcbc354e925f13e121cd2
badriysha <badriysha@gmail.com>
2021-04-28 15:11:20 +0700

    menginstall package hapi dan membuat server baru

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
