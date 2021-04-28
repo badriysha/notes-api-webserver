2ee7276c452855c9675341b3d8708e255d87d4ee
badriysha <badriysha@gmail.com>
2021-04-29 06:30:22 +0700

    menampilkan data note & data by id

--- src/handler.js
+++ src/handler.js
@@ -2,26 +2,27 @@ const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
-   const { title, tag, body } = request.payload;
+   const { title, tags, body } = request.payload;
 
    const id = nanoid(16);
    const createdAt = new Date().toISOString();
    const updatedAt = createdAt;
 
    const newNote = {
-      title, tag, body, id, createdAt, updatedAt
-   }
+      title, tags, body, id, createdAt, updatedAt,
+   };
 
    notes.push(newNote);
 
    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
    if (isSuccess) {
       const response = h.response({
          status: 'success',
          message: 'Catatan berhasil ditambahkan',
          data: {
             noteId: id,
-         }
+         },
       });
       response.code(201);
       return response;
@@ -35,4 +36,33 @@ const addNoteHandler = (request, h) => {
    return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+   status: 'success',
+   data: {
+      notes,
+   },
+});
+
+const getNoteByIdHandler = (request, h) => {
+   const { id } = request.params;
+
+   const note = notes.filter((n) => n.id === id)[0];
+
+   if (note !== undefined) {
+      return {
+         status: 'success',
+         data: {
+            note,
+         }
+      }
+   }
+
+   const response = h.response({
+      status: 'fail',
+      message: 'Catatan tidak ditemukan',
+   });
+   response.code(404);
+   return response;
+}
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
